751f1fec4dc7912ab81189e371de6a233f2e6e30
tomysshadow <ellescuba@gmail.com>
2017-04-16 00:59:27 -0600

    no message

---
+++ NetJetController/NetJetController.cpp
@@ -0,0 +1,514 @@
+#include <windows.h>
+#include "NetJetController.h"
+#include "myXInput.h"
+
+/*
+#include <string.h>
+#include <fstream>
+#include <iostream>
+*/
+
+//#pragma comment(lib, "XInput.lib")
+
+HINSTANCE originalNetJetController;
+HINSTANCE original360Controller;
+//std::basic_ofstream<char> f;
+NetJetEmulator NJ;
+NetJetEmulator::Keyboard KB;
+
+
+
+
+
+
+
+
+LRESULT __stdcall NetJetEmulator::Keyboard::backgroundThread(int nCode, WPARAM wParam, LPARAM lParam) {
+	// event handler for when keyboard does something
+	if (nCode >= 0) {
+		// if onKeyDown or onKeyUp
+		if (wParam == WM_KEYDOWN || wParam == WM_KEYUP) {
+			// get reference to the system's keyboard
+			KB.keyboard = *((KBDLLHOOKSTRUCT*)lParam);
+			for (byte i = 0;i < keyslength;i++) {
+				// if the keyCode matches one of our keycodes
+				if (KB.keyboard.vkCode == KB.keycodes[i]) {
+					// reflect this in our keysdown array
+					KB.keysdown[i] = (wParam == WM_KEYDOWN);
+					break;
+				}
+			}
+		}
+	}
+	// continue watching the keyboard
+	return CallNextHookEx(KB.backgroundThreadHook, nCode, wParam, lParam);
+}
+
+void NetJetEmulator::SetState(int *a, bool down, int mapping, bool override=false) {
+	// if the key/button is down
+	if (down)
+	{
+		if (!override) {
+			// set our mapping
+			*a |= mapping;
+		} else {
+			// set our mapping, ignoring previous value (i.e. don't go faster when both arrow keys and DPad is pressed etc.)
+			// don't use this on Gamepad_wButtons, it will have the effect of clearing all other Gamepad_wButtons
+			*a  = mapping;
+		}
+	}
+}
+
+void NetJetEmulator::SetControllerInserted(int *Gamepad_wButtons, int *Gamepad_bThumbRX, int *Gamepad_bThumbRY, bool result = true, bool override = false, bool overridejoysticks = false) {
+	bool down = (!result || (*Gamepad_wButtons & 0x00010000) != 0x00010000);
+	// override previous value
+	// considering the controller is apparently not inserted and it could have been anything
+	SetState(Gamepad_wButtons, down, 0x00010000, override);
+	// centre the joysticks
+	// considering the controller was apparently not inserted
+	SetState(Gamepad_bThumbRX, down, 0x0000001F, overridejoysticks);
+	SetState(Gamepad_bThumbRY, down, 0x0000001F, overridejoysticks);
+}
+
+void NetJetEmulator::SetCartridgeInserted(int *Gamepad_wButtons, bool override = false) {
+	bool down = ((*Gamepad_wButtons & 0x00100080) != 0x00100080);
+	SetState(Gamepad_wButtons, down, 0x00100080, override);
+}
+
+void NetJetEmulator::FixJoysticks(int *Gamepad_bThumbRX, int *Gamepad_bThumbRY) {
+	if (*Gamepad_bThumbRX < 0) {
+		*Gamepad_bThumbRX = 0;
+	}
+	if (*Gamepad_bThumbRX > 64) {
+		*Gamepad_bThumbRX = 64;
+	}
+	if (*Gamepad_bThumbRY < 0) {
+		*Gamepad_bThumbRY = 0;
+	}
+	if (*Gamepad_bThumbRY > 64) {
+		*Gamepad_bThumbRY = 64;
+	}
+}
+
+void NetJetEmulator::callNetJetControllerGetState(int *Gamepad_wButtons, int *Gamepad_bThumbRX, int *Gamepad_bThumbRY, bool result) {
+	// call intercepted
+	//POINT curpos;
+	//int myWidth = GetSystemMetrics(SM_CXSCREEN);
+	//int myHeight = GetSystemMetrics(SM_CYSCREEN);
+	XInputGetState_ originalXInputGetState;
+	XINPUT_STATE the360ControllerState;
+	DWORD theXbox360ControllerInserted;
+
+	/*
+	if (f.is_open()) {
+		f << "NetJetControllerGetState " << *Gamepad_wButtons << " " << *Gamepad_bThumbRX << " " << *Gamepad_bThumbRY << "\n";
+	}
+	*/
+
+	// ensure the game thinks a controller is inserted
+	SetControllerInserted(Gamepad_wButtons, Gamepad_bThumbRX, Gamepad_bThumbRY, result, true, true);
+	// ensure the game thinks a cartridge is inserted and valid
+	SetCartridgeInserted(Gamepad_wButtons);
+	// if the controller isn't suspended...
+	if (!suspended) {
+		// ignore 360 Controller if user doesn't have XInput
+		if (!original360Controller) {
+		} else {
+			// get the 360 Controller state
+			memset(&the360ControllerState, 0x00, sizeof(XINPUT_STATE));
+			originalXInputGetState = (XInputGetState_)GetProcAddress(original360Controller, "XInputGetState");
+			if (!originalXInputGetState) {
+				// do this here to avoid out of date values
+				theXbox360ControllerInserted = 1167L;
+			}
+			else {
+				theXbox360ControllerInserted = originalXInputGetState(0, &the360ControllerState);
+			}
+			// if the 360 Controller is inserted
+			if (theXbox360ControllerInserted == 0L) {
+				// set currently pressed Gamepad_wButtons as down on NetJet Controller
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x8000, 0x00000001);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x2000, 0x00000002);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x4000, 0x00000004);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x1000, 0x00000008);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.bLeftTrigger  > 0x0000, 0x00000010);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.bRightTrigger > 0x0000, 0x00000020);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x0100, 0x00000010);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x0200, 0x00000020);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x0010, 0x00000100);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.sThumbLY      >   7849, 0x00000200);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.sThumbLY      <  -7849, 0x00000400);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.sThumbLX      <  -7849, 0x00000800);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.sThumbLX      >   7849, 0x00001000);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x0001, 0x00000200);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x0002, 0x00000400);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x0004, 0x00000800);
+				SetState(Gamepad_wButtons, the360ControllerState.Gamepad.wButtons      & 0x0008, 0x00001000);
+				if (the360ControllerState.Gamepad.sThumbRX < -8689 || the360ControllerState.Gamepad.sThumbRX > 8689) {
+					*Gamepad_bThumbRX = (the360ControllerState.Gamepad.sThumbRX / 65536.0 + 0.5) * 64.0;
+				}
+				if (the360ControllerState.Gamepad.sThumbRY < -8689 || the360ControllerState.Gamepad.sThumbRY > 8689) {
+					*Gamepad_bThumbRY = (-the360ControllerState.Gamepad.sThumbRY / 65536.0 + 0.5) * 64.0;
+				}
+			}
+		}
+
+		// if the game isn't already key mapping for us...
+		if (!keymapping) {
+			// set currently pressed keys as down on NetJet Controller
+			// Button 4
+			SetState(Gamepad_wButtons, KB.keysdown[15], 0x00000001);
+			SetState(Gamepad_wButtons, KB.keysdown[0] , 0x00000001);
+			// Button 2
+			SetState(Gamepad_wButtons, KB.keysdown[18], 0x00000002);
+			SetState(Gamepad_wButtons, KB.keysdown[16], 0x00000002);
+			SetState(Gamepad_wButtons, KB.keysdown[1] , 0x00000002);
+			// Button 3
+			SetState(Gamepad_wButtons, KB.keysdown[17], 0x00000004);
+			SetState(Gamepad_wButtons, KB.keysdown[2] , 0x00000004);
+			// Button 1
+			SetState(Gamepad_wButtons, KB.keysdown[20], 0x00000008);
+			SetState(Gamepad_wButtons, KB.keysdown[19], 0x00000008);
+			SetState(Gamepad_wButtons, KB.keysdown[3] , 0x00000008);
+			// Left Shoulder
+			SetState(Gamepad_wButtons, KB.keysdown[21], 0x00000010);
+			SetState(Gamepad_wButtons, KB.keysdown[4] , 0x00000010);
+			// Right Shoulder
+			SetState(Gamepad_wButtons, KB.keysdown[22], 0x00000020);
+			SetState(Gamepad_wButtons, KB.keysdown[5] , 0x00000020);
+			// Start
+			SetState(Gamepad_wButtons, KB.keysdown[23], 0x00000100);
+			SetState(Gamepad_wButtons, KB.keysdown[6] , 0x00000100);
+			// DPad Up
+			SetState(Gamepad_wButtons, KB.keysdown[7] , 0x00000200);
+			// DPad Down
+			SetState(Gamepad_wButtons, KB.keysdown[8] , 0x00000400);
+			// DPad Left
+			SetState(Gamepad_wButtons, KB.keysdown[9] , 0x00000800);
+			// DPad Right
+			SetState(Gamepad_wButtons, KB.keysdown[10], 0x00001000);
+			// Right Thumbstick
+			SetState(Gamepad_bThumbRY, KB.keysdown[11], 0x00000000, true);
+			SetState(Gamepad_bThumbRX, KB.keysdown[12], 0x00000000, true);
+			SetState(Gamepad_bThumbRY, KB.keysdown[13], 0x00000040, true);
+			SetState(Gamepad_bThumbRX, KB.keysdown[14], 0x00000040, true);
+		}
+
+		// ignore mouse mapping because game conflicts with it
+		/*
+		if (!mousemapping) {
+			if (GetCursorPos(&curpos)) {
+				if (myWidth > 0 && (curpos.x - (myWidth / 2)) != 0) {
+					*Gamepad_bThumbRX = 31 + ((curpos.x - (myWidth / 2)) / (double)myWidth * 31);
+				}
+				if (myHeight > 0 && (curpos.y - (myHeight / 2)) != 0) {
+					*Gamepad_bThumbRY = 31 + ((curpos.y - (myHeight / 2)) / (double)myHeight * 31);
+				}
+			}
+			SetCursorPos(myWidth / 2, myHeight / 2);
+		}
+		*/
+	}
+
+	// ensure joysticks are not further up/down/left/right than they can be on the controller
+	FixJoysticks(Gamepad_bThumbRX, Gamepad_bThumbRY);
+	// ensure the game thinks a controller is inserted and a cartridge is inserted and valid one more time just in case
+	SetControllerInserted(Gamepad_wButtons, Gamepad_bThumbRX, Gamepad_bThumbRY, true, false, false);
+	SetCartridgeInserted(Gamepad_wButtons);
+}
+
+void NetJetEmulator::callNetJetControllerGetKey(char *Gamepad_wButtons, bool result) {
+	// call intercepted
+
+	/*
+	if (f.is_open()) {
+		f << "NetJetControllerGetState " << *Gamepad_wButtons << " " << *Gamepad_bThumbRX << " " << *Gamepad_bThumbRY << "\n";
+	}
+	*/
+
+	// zero key, this is not a keygen
+	// replicating real keys is outside the scope of this project
+	if (!result) {
+		memset(Gamepad_wButtons, 0x00, 0x20);
+	}
+}
+
+
+
+
+
+
+
+
+BOOL WINAPI DllMain(HINSTANCE hInst, DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch (fdwReason) {
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hInst);
+		originalNetJetController = LoadLibraryA("NetJetController_.DLL");
+		if (!originalNetJetController) {
+			return false;
+		}
+
+		/*
+		remove("console_NetJetController.log");
+		f.open("console_NetJetController.log", std::ios::out | std::ios::app);
+		*/
+
+		KB.backgroundThreadHook = SetWindowsHookEx(WH_KEYBOARD_LL, KB.backgroundThread, NULL, 0);
+		ShowCursor(false);
+		break;
+	case DLL_PROCESS_DETACH:
+		FreeLibrary(originalNetJetController);
+
+		/*
+		if (f.is_open()) {
+			f.close();
+		}
+		*/
+
+		UnhookWindowsHookEx(KB.backgroundThreadHook);
+		ShowCursor(true);
+		break;
+	}
+	return true;
+}
+
+// duplicate functions which call the original and return false
+// we always want these calls to appear successful
+// oddly returning false means no error ocurred
+typedef signed int(*NetJetControllerEnableKeyMapping_)();
+extern "C" signed int callNetJetControllerEnableKeyMapping()
+{
+	NetJetControllerEnableKeyMapping_ originalNetJetControllerEnableKeyMapping;
+
+	originalNetJetControllerEnableKeyMapping = (NetJetControllerEnableKeyMapping_)GetProcAddress(originalNetJetController, "NetJetControllerEnableKeyMapping");
+	if (!originalNetJetControllerEnableKeyMapping) {
+	} else {
+		originalNetJetControllerEnableKeyMapping();
+	}
+
+	NJ.keymapping = true;
+	UnhookWindowsHookEx(KB.backgroundThreadHook);
+
+	return 0;
+}
+
+typedef signed int(*NetJetControllerDisableKeyMapping_)();
+extern "C" signed int callNetJetControllerDisableKeyMapping()
+{
+	NetJetControllerDisableKeyMapping_ originalNetJetControllerDisableKeyMapping;
+
+	originalNetJetControllerDisableKeyMapping = (NetJetControllerDisableKeyMapping_)GetProcAddress(originalNetJetController, "NetJetControllerDisableKeyMapping");
+	if (!originalNetJetControllerDisableKeyMapping) {
+	} else {
+		originalNetJetControllerDisableKeyMapping();
+	}
+
+	NJ.keymapping = false;
+	KB.backgroundThreadHook = SetWindowsHookEx(WH_KEYBOARD_LL, KB.backgroundThread, NULL, 0);
+
+	return 0;
+}
+
+typedef signed int(*NetJetControllerEnableMouseMapping_)();
+extern "C" signed int callNetJetControllerEnableMouseMapping()
+{
+	NetJetControllerEnableMouseMapping_ originalNetJetControllerEnableMouseMapping;
+
+	originalNetJetControllerEnableMouseMapping = (NetJetControllerEnableMouseMapping_)GetProcAddress(originalNetJetController, "NetJetControllerEnableMouseMapping");
+	if (!originalNetJetControllerEnableMouseMapping) {
+	} else {
+		originalNetJetControllerEnableMouseMapping();
+	}
+
+	NJ.mousemapping = true;
+	ShowCursor(true);
+
+	return 0;
+}
+
+typedef signed int(*NetJetControllerDisableMouseMapping_)();
+extern "C" signed int callNetJetControllerDisableMouseMapping()
+{
+	NetJetControllerDisableMouseMapping_ originalNetJetControllerDisableMouseMapping;
+
+	originalNetJetControllerDisableMouseMapping = (NetJetControllerDisableMouseMapping_)GetProcAddress(originalNetJetController, "NetJetControllerDisableMouseMapping");
+	if (!originalNetJetControllerDisableMouseMapping) {
+	} else {
+		originalNetJetControllerDisableMouseMapping();
+	}
+
+	NJ.mousemapping = false;
+	ShowCursor(false);
+
+	return 0;
+}
+
+typedef signed int(*NetJetControllerInitialize_)();
+extern "C" signed int callNetJetControllerInitialize()
+{
+	NetJetControllerInitialize_ originalNetJetControllerInitialize;
+
+	originalNetJetControllerInitialize = (NetJetControllerInitialize_)GetProcAddress(originalNetJetController, "NetJetControllerInitialize");
+	if (!originalNetJetControllerInitialize) {
+	} else {
+		originalNetJetControllerInitialize();
+	}
+
+	original360Controller = LoadLibraryA("XINPUT9_1_0.DLL");
+
+	return 0;
+}
+
+typedef BOOL(*NetJetControllerSuspend_)();
+extern "C" BOOL callNetJetControllerSuspend()
+{
+	NetJetControllerSuspend_ originalNetJetControllerSuspend;
+
+	originalNetJetControllerSuspend = (NetJetControllerSuspend_)GetProcAddress(originalNetJetController, "NetJetControllerSuspend");
+	if (!originalNetJetControllerSuspend) {
+	} else {
+		originalNetJetControllerSuspend();
+	}
+	NJ.suspended = true;
+
+	return false;
+}
+
+typedef BOOL(*NetJetControllerResume_)();
+extern "C" BOOL callNetJetControllerResume()
+{
+	NetJetControllerResume_ originalNetJetControllerResume;
+
+	originalNetJetControllerResume = (NetJetControllerSuspend_)GetProcAddress(originalNetJetController, "NetJetControllerResume");
+	if (!originalNetJetControllerResume) {
+	} else {
+		originalNetJetControllerResume();
+	}
+	NJ.suspended = false;
+
+	return false;
+}
+
+typedef signed int(*NetJetControllerShutdown_)();
+extern "C" signed int callNetJetControllerShutdown()
+{
+	NetJetControllerShutdown_ originalNetJetControllerShutdown;
+
+	originalNetJetControllerShutdown = (NetJetControllerShutdown_)GetProcAddress(originalNetJetController, "NetJetControllerShutdown");
+	if (!originalNetJetControllerShutdown) {
+	} else {
+		originalNetJetControllerShutdown();
+	}
+
+	return 0;
+}
+
+typedef signed int(*NetJetControllerSetKeyMapping_)(INT16);
+extern "C" signed int callNetJetControllerSetKeyMapping(INT16 Gamepad_wButtons)
+{
+	NetJetControllerSetKeyMapping_ originalNetJetControllerSetKeyMapping;
+
+	originalNetJetControllerSetKeyMapping = (NetJetControllerSetKeyMapping_)GetProcAddress(originalNetJetController, "NetJetControllerSetKeyMapping");
+	if (!originalNetJetControllerSetKeyMapping) {
+	} else {
+		originalNetJetControllerSetKeyMapping(Gamepad_wButtons);
+	}
+
+	return 0;
+}
+
+typedef BOOL(__cdecl *NetJetControllerSetOption_)(int, int);
+extern "C" BOOL __cdecl callNetJetControllerSetOption(int Gamepad_wButtons, int nPriority)
+{
+	NetJetControllerSetOption_ originalNetJetControllerSetOption;
+
+	originalNetJetControllerSetOption = (NetJetControllerSetOption_)GetProcAddress(originalNetJetController, "NetJetControllerSetOption");
+	if (!originalNetJetControllerSetOption) {
+	} else {
+		originalNetJetControllerSetOption(Gamepad_wButtons, nPriority);
+	}
+
+	return false;
+}
+
+typedef BOOL(__cdecl *NetJetControllerGetState_)(int, int, int);
+extern "C" BOOL __cdecl callNetJetControllerGetState(int Gamepad_wButtons, int Gamepad_bThumbRX, int Gamepad_bThumbRY)
+{
+	BOOL result = false;
+	NetJetControllerGetState_ originalNetJetControllerGetState;
+
+	originalNetJetControllerGetState = (NetJetControllerGetState_)GetProcAddress(originalNetJetController, "NetJetControllerGetState");
+	if (!originalNetJetControllerGetState) {
+	}
+	else {
+		result = !originalNetJetControllerGetState(Gamepad_wButtons, Gamepad_bThumbRX, Gamepad_bThumbRY);
+	}
+
+	NJ.callNetJetControllerGetState((int*)Gamepad_wButtons, (int*)Gamepad_bThumbRX, (int*)Gamepad_bThumbRY, result);
+
+	return false;
+}
+
+typedef BOOL(__cdecl *NetJetControllerSetWindow_)(HWND);
+extern "C" BOOL __cdecl callNetJetControllerSetWindow(HWND hWnd)
+{
+	NetJetControllerSetWindow_ originalNetJetControllerSetWindow;
+
+	originalNetJetControllerSetWindow = (NetJetControllerSetWindow_)GetProcAddress(originalNetJetController, "NetJetControllerSetWindow");
+	if (!originalNetJetControllerSetWindow) {
+	} else {
+		originalNetJetControllerSetWindow(hWnd);
+	}
+
+	return false;
+}
+
+typedef BOOL(__cdecl *NetJetControllerGetControllerKey_)(int);
+extern "C" BOOL __cdecl callNetJetControllerGetControllerKey(int Gamepad_wButtons)
+{
+	BOOL result = false;
+	NetJetControllerGetControllerKey_ originalNetJetControllerGetControllerKey;
+
+	originalNetJetControllerGetControllerKey = (NetJetControllerGetControllerKey_)GetProcAddress(originalNetJetController, "NetJetControllerGetControllerKey");
+	if (!originalNetJetControllerGetControllerKey) {
+	} else {
+		result = !originalNetJetControllerGetControllerKey(Gamepad_wButtons);
+	}
+
+	NJ.callNetJetControllerGetKey((char *)Gamepad_wButtons, result);
+
+	return false;
+}
+
+typedef BOOL(__cdecl *NetJetControlleretCartrdigeKey_)(int);
+extern "C" BOOL __cdecl callNetJetControlleretCartrdigeKey(int Gamepad_wButtons)
+{
+	BOOL result = false;
+	NetJetControlleretCartrdigeKey_ originalNetJetControlleretCartrdigeKey;
+
+	originalNetJetControlleretCartrdigeKey = (NetJetControlleretCartrdigeKey_)GetProcAddress(originalNetJetController, "NetJetControlleretCartrdigeKey");
+	if (!originalNetJetControlleretCartrdigeKey) {
+	} else {
+		result = !originalNetJetControlleretCartrdigeKey(Gamepad_wButtons);
+	}
+
+	NJ.callNetJetControllerGetKey((char *)Gamepad_wButtons, result);
+
+	return false;
+}
+
+typedef signed int(*NetJetControllerRun_)();
+extern "C" signed int callNetJetControllerRun()
+{
+	NetJetControllerRun_ originalNetJetControllerRun;
+
+	originalNetJetControllerRun = (NetJetControllerRun_)GetProcAddress(originalNetJetController, "NetJetControllerRun");
+	if (!originalNetJetControllerRun) {
+	} else {
+		originalNetJetControllerRun();
+	}
+
+	return 0;
+}
---
+++ NetJetController/readme.txt
@@ -0,0 +1,81 @@
+3D Groove NetJet Emulator
+Programmed by TOMYSSHADOW
+--------
+Long Description:
+	An emulator for the Hasbro NetJet Controller device/QiGO/USBKEY meant specifically for use with Transformers Battle Universe. Allows the use of your keyboard/360 Controller in place of the NetJet Controller. If a legitimate NetJet Controller is connected, it will still use that controller. If a 360 Controller is connected, it will still use the NetJet Controller, but prefer input from the 360 Controller. Keyboard input will be preferred over either controller. Button presses are not cumulative (pressing left on both the NetJet Controller and keyboard will not make you run twice as fast in-game.) A game key is still required, this is not a keygen. While one could theoretically remove the Armadillo 4.66 DRM used by NetJet games and they would work without any legitimate NetJet Controller, that is outside the scope of this project.
+
+	This is a proxy DLL to reside in the game directory. It relies on the original NetJetController.DLL being present but renamed to NetJetController_.DLL. SetWindowsHookEx is used to prevent the game from receiving keystrokes. This is to prevent key/button presses being received twice.
+
+	In the future this code may be recycled into a fully fledged usermode application to pair with a NetJet Emulator driver that would give a universal emulation. The driver could reasonably be based off of FileDisk, as it would be required to mount an ISO image or similar. This would require returning a buffer in the NetJet Controller's device language from DeviceIoControl. The NetJet Controller exposes a CDROM interface. It mounts an image containing an AutoRun.exe and autorun.inf file which downloads an installer for the keyhole application for NetJet games via the internet. The Hasbro server for this is no longer available, and an error is given if attempting to connect to it. It does not expose any HID (Human Interface Device) interface. Instead, DeviceIoControl is used to Get Controller State as ReadFile/WriteFile would imply reading from/writing to the disk. Games identify the NetJet Controller by its Vendor ID (VID) and Product ID (PID.)
+--------
+Maps:
+	Controller Inserted: Always
+	Cartridge Inserted: Always, if legitimate NetJet Controller does not have a key in, a dummy key is returned
+
+	Keyboard Map:
+	1, Enter, Space     - Button 1
+	2, Q    , Backspace - Button 2
+	3, E                - Button 3
+	4, F                - Button 4
+	L, Left Shift       - Left Shoulder
+	R, Right Shift      - Right Shoulder
+	0, Escape           - Start
+	W                   - DPad Up
+	A                   - DPad Left
+	S                   - DPad Down
+	D                   - DPad Up
+	Up Arrow Key        - Right Thumbstick Up
+	Down Arrow Key      - Right Thumbstick Down
+	Left Arrow Key      - Right Thumbstick Left
+	Right Arrow Key     - Right Thumbstick Right
+
+	360 Controller Map:
+	A                                 - Button 1
+	B                                 - Button 2
+	X                                 - Button 3
+	Y                                 - Button 4
+	Left Button, Left Trigger         - Left Shoulder
+	Right Button, Right Trigger       - Right Shoulder
+	Start                             - Start
+	DPad Up   , Left Thumbstick Up    - DPad Up
+	DPad Down , Left Thumbstick Down  - DPad Down
+	DPad Left , Left Thumbstick Left  - DPad Left
+	DPad Right, Left Thumbstick Right - DPad Right
+	Right Thumbstick Up               - Right Thumbstick Up
+	Right Thumbstick Down             - Right Thumbstick Down
+	Right Thumbstick Left             - Right Thumbstick Left
+	Right Thumbstick Right            - Right Thumbstick Right
+--------
+Device Details:
+	Friendly Name:
+	Net Jet Controller USB Device
+
+	Parent:
+	USB\VID_3078&PID_B061\6&85ca75f&0&6
+
+	IoControlCodes:
+	4D014h - Get Controller State
+
+	OutBuffer:
+		DWORD Gamepad_wButtons
+		Cartridge_Inserted Controller_Inserted "Right Shoulder" "Left Shoulder" "Button One" "Button Three" "Button Two" "Button Four"
+		Mouse_Map??        Unknown             Unknown          "DPad Right"    "DPad Left"  "DPad Down"    "DPad Up"    "Start"
+
+		byte Gamepad_bThumbRX
+		Right Analog Stick Left to Right
+
+		byte Gamepad_bThumbRY
+		Right Analog Stick Up to Down
+
+		byte[20] ControllerKey
+		Controller Key
+
+		byte[20] CartridgeKey
+		Cartridge Key
+
+	Messages:
+	401h - WM_CONTROLLER_INSERTED
+	402h - WM_CONTROLLER_REMOVED
+	403h - WM_CARTRIDGE_INSERTED
+	404h - WM_CARTRIDGE_REMOVED
+	405h - WM_CONTROLLER_INVALID
